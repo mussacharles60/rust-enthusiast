1689b4e59425cf5bd9b8d38bb448226d51dec30c
Mussa Charles <mussacharles60@gmail.com>
2023-01-01 20:56:27 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use std::io;
+
 // fn add_numbers(x: i32, y: i32) -> i32 {
 //     // println!("the sum is: {}", x + y);
 //     x + y
 // }
 
-fn add_numbers2(x: i32, y: i32) -> i32 {
-    // println!("the sum is: {}", x + y);
-    return x + y;
-}
+// fn add_numbers2(x: i32, y: i32) -> i32 {
+//     // println!("the sum is: {}", x + y);
+//     return x + y;
+// }
 
 fn main() {
     println!("Hello world!");
+
+    // prelude
+    let mut input = String::new();  // create new empty string
+    // get standard input
+    io::stdin().read_line(/*reference*/&mut input)
+    // catch
+    .expect("failed to real line");
+    println!("{}", input);
+
+
     // let food = "gg";
     // if food == "cookie" {
     //     println!("I like {}", food);
@@ -30,6 +42,71 @@ fn main() {
 
     // println!("{}", number);
 
-    let result = add_numbers2(23, 39);
-    println!("{}", result);
+    // let result = add_numbers2(23, 39);
+    // println!("{}", result);
+
+    // loops
+
+    // // loop {
+    // //     println!("dude again!");
+    // // }
+
+    // let mut counter = 0;
+
+    // let result = loop {
+    //     counter += 1;
+    //     println!("counter: {counter}");
+
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+
+    // println!("The result is {result}");
+
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {count}");
+    //     let mut remaining = 10;
+
+    //     loop {
+    //         println!("remaining = {remaining}");
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -= 1;
+    //     }
+
+    //     count += 1;
+    // }
+    // println!("End count = {count}");
+
+    // let mut number = 3;
+
+    // while number != 0 {
+    //     println!("{number}!");
+
+    //     number -= 1;
+    // }
+
+    // println!("LIFTOFF!!!");
+
+    // let a = [10, 20, 30, 40, 50];
+    // let mut index = 0;
+
+    // while index < 5 {
+    //     println!("the value is: {}", a[index]);
+
+    //     index += 1;
+    // }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
 }
